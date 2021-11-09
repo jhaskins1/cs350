56e5f058afda392b0905da383a47f8c9352c7c0c
jhaskins1 <jhaskins1@hawk.iit.edu>
2021-11-09 03:00:23 -0600

    Finished lab 4

--- cs350-lab4/CS350-lab4.s
+++ cs350-lab4/CS350-lab4.s
@@ -1,6 +1,5 @@
 #Reserve space in memory for two variables called var1 and var2 of size “word” (use the ‘.word’ assembly directive).
-#Also, reserve space in the memory for two variables called ext1 and ext2 of size “word” (use the ‘.extern’ assembly directive,
-#the assembler will reserve space for them in the data segment that can be accessed using the $gp register).
+#Also, reserve space in the memory for two variables called ext1 and ext2 of size “word” (use the ‘.extern’ assembly directive)
 #This program copies the values of var1 and var2 in ext2 and ext1 respectively
 
     .data
@@ -12,11 +11,20 @@ var2: .word 0xffff
     .text
     .globl main
 main:
-    #load the vars into registers
-    la $t0, var1
-    lw $t1, 0($t0)
+    #load numbers from vars into registers
+    la $t0, var1 #load address of var1 into $t0
+    lw $t1, 0($t0) #load the content of 0($t0) into $t1
 
-    la $t1, var2
-    lw $t2, 0($t1)
+    la $t0, var2 #load address of var2 into $t0
+    lw $t2, 0($t0) #load the content of 0($t0) into $t2
 
-    
+    #store the number in global space
+    la $t0, ext2
+    sw $t1, 0($t0)
+
+    la $t0, ext1
+    sw $t2, 0($t0)
+
+    #exit gracefully
+    li $v0, 10
+    syscall
